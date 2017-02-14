116810ea2d68b5333f6a6671f605d41229861e02
Vasanth Sadhasivan <Vasanth Sadhasivan>
2017-02-13 20:54:56 -0800

    fixed db

--- query.php
+++ query.php
@@ -1,20 +1,24 @@
 <?php
 	session_start();
 
-	$db = mysqli_connect("198.71.235.82", "zachaodsaofjiads", "aaaaaaaa", "authentication");
+	$db = mysqli_connect("localhost", "orhx_web_user1", "orhx_web_user1", "ORHX Sign Ups");
 	if (mysqli_connect_errno()) {
   		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
 	if (isset($_POST['btn'])){
-		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$fullname = mysqli_real_escape_string($db, $_POST['fullname']);
 		
 		$email = mysqli_real_escape_string($db, $_POST['email']);
 
-		$pass = mysqli_real_escape_string($db, $_POST['pass']);
+		$tshirt = mysqli_real_escape_string($db, $_POST['tshirt']);
 
-		$id = mysqli_real_escape_string($db, $_POST['id']);
+		$gender = mysqli_real_escape_string($db, $_POST['gender']);
+		
+		$priorinfo = mysqli_real_escape_string($db, $_POST['priorinfo']);
+		
+		$diet = mysqli_real_escape_string($db, $_POST['diet']);
 
-		$command = "INSERT INTO users(username, email, pass, id) VALUES('$username','$email', '$pass', '$id')";
+		$command = "INSERT INTO Data(fullname, email, tshirt, gender, priorinfo, diet) VALUES('$fullname','$email', '$tshirt', '$gender', '$priorinfo', '$diet')";
 
 		if (!mysqli_query($db,$command)) {
   			die('Error: ' . mysqli_error($db));
